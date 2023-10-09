eccf4d50605e67d887af5aeee5c589f31edb29f3
Gautham <gauthamkrishnanpriya@gmail.com>
2023-10-09 04:51:27 +0000

    add: gradio interface

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ import torch
 from tokenizers import Tokenizer
 from model import *
 from inference import get_translation
-model = torch.load('opus_books_weights/tmodel_final_model_weights_en-it.pt')
+import gradio as gr
+
+device = torch.device('cpu') # setting device to cpu to run of huggingface spaces free tier
+
+model = torch.load('opus_books_weights/tmodel_final_model_weights_en-it.pt').to(device)
 src_tokenizer = Tokenizer.from_file('tokenizer_en.json')
 tgt_tokenizer = Tokenizer.from_file('tokenizer_it.json')
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 seq_len = 350
 
-text = input("Enter a sentence in English: ")
+def translate(text):
+    translation = get_translation(model, text, src_tokenizer, tgt_tokenizer, seq_len, device)
+    return translation
 
-translation = get_translation(model, text, src_tokenizer, tgt_tokenizer, seq_len, device)
+title = "English to Italian Translator"
+description = "A simple English to Italian translator using a Transformer model trained on the OPUS Books dataset."
 
-print("Translation: ", translation)
+gr.Interface(fn=translate, inputs="text", outputs="text", title=title, description=description).launch()
